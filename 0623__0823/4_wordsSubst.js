09997baadf4b846bb6e0c95f82e64b7ea31cb6e2
MrDrog <fireman3085@mail.ru>
2023-06-06 15:49:34 +0300

    feature: new problems: 5-6

--- 0623__0823/4_wordsSubst.js
+++ 0623__0823/4_wordsSubst.js
@@ -1,10 +1,10 @@
 //get string and return string with word "test" instead each word in initial string (codewars)
 
-function volCub(string) {
+function waterNeed(string) {
     const length = string.split(' ').length
     return string === ''? '':new Array(length).fill('test').join(' ')
 }
 
-console.log(volCub('I will go to shop today')) //6
-console.log(volCub('How I test this problem if not like this')) //9
-console.log(volCub('')) //
+console.log(waterNeed('I will go to shop today')) //6
+console.log(waterNeed('How I test this problem if not like this')) //9
+console.log(waterNeed('')) //
