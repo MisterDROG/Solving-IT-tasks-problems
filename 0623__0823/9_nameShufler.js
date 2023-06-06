ac0c83cae5a6ee3556332c16c6226fb971080827
MrDrog <fireman3085@mail.ru>
2023-06-06 19:54:40 +0300

    feature: new problems: 8 & 9

---
+++ 0623__0823/9_nameShufler.js
@@ -0,0 +1,26 @@
+//Write a function that returns a string in which firstname is swapped with last name.
+
+function nameShuffler(name) {
+    return name.split(' ').reverse().join(' ')
+}
+
+console.log(nameShuffler("Idwal Augustin")) // Augustin Idwal
+
+//without arrays
+function nameShuffler2(name) {
+    let resSec = ''
+    let resName = ''
+    let reverse = false
+    for (let i=0; i < name.length; i++ ) {
+        if (name[i] === ' ') {
+            reverse = true
+        } else if (reverse) {
+            resSec = resSec + name[i]
+        } else {
+            resName = resName + name[i]
+        }
+    }
+    return `${resSec} ${resName}`
+}
+
+console.log(nameShuffler2("Idwal Augustin")) // Augustin Idwal
