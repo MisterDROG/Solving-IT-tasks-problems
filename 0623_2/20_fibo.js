247b501b53c4efac13eaa9af241d14522046b4f6
MrDrog <fireman3085@mail.ru>
2023-06-16 14:36:58 +0300

    feature: new problems 20

--- 0623_2/20_fibo.js
+++ 0623_2/20_fibo.js
@@ -3,10 +3,15 @@
 function fibonacci(n) {
     const seq = [1,1]
 
-    while (seq.length<n) {
-        seq.push(seq[-1] + )
-    }
+    if (n<2) return seq.slice(0, n)
 
+    while (seq.length<n) {
+        seq.push(seq[seq.length-1] + seq[seq.length-2])
+    }
+    return seq
   }
   
   console.log(fibonacci(8)) // -> [1, 1, 2, 3, 5, 8, 13, 21]
+  console.log(fibonacci(1)) // [ 1 ]
+  console.log(fibonacci(2)) // [ 1, 1 ]
+  console.log(fibonacci(0)) // []
