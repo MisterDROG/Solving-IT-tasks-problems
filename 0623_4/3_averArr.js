2e03cfc9324feb5945c755989f33ab12096dbb9f
MrDrog <fireman3085@mail.ru>
2023-06-29 14:56:26 +0300

    new problems: add problem 4

--- 0623_4/3_averArr.js
+++ 0623_4/3_averArr.js
@@ -1,6 +1,10 @@
 //find avarage of array
 
+const arr = [1, 3, 5, 7 , 8, 12]
+
 function findAverage(array) {
     if (array.length == 0) return 0
     return array.reduce((sum, el) => sum+el)/array.length
   }
+
+console.log(findAverage(arr)) //6
