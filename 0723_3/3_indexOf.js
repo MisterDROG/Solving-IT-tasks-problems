c16ae74bb3d8b76846427f500e707b45feb31378
MrDrog <fireman3085@mail.ru>
2023-07-19 19:54:20 +0300

    new problem: add new problem 3

---
+++ 0723_3/3_indexOf.js
@@ -0,0 +1,11 @@
+//What in console?
+
+const groceries = ['banana', 'apple', 'peanuts'];
+
+if (groceries.indexOf('banana')) {
+    console.log('We have to buy bananas!');
+} else {
+    console.log('We dont have to buy bananas!');
+}
+
+//We dont have to buy bananas!
