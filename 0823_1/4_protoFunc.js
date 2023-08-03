03e52556309777fe9cdd9bbfb6815c82aa5c060b
MrDrog <fireman3085@mail.ru>
2023-08-03 21:05:14 +0300

    new problem: add new problem 0823_1_4

---
+++ 0823_1/4_protoFunc.js
@@ -0,0 +1,11 @@
+//What will be in console?
+
+String.prototype.givePizza = () => {
+    return "Just give me some Pizza"
+}
+
+const nameN = "Mark"
+
+console.log(nameN.givePizza())
+
+// "Just give me some Pizza"
