1da3e08a7c67c483d654e770361d7af283ab087a
MrDrog <fireman3085@mail.ru>
2023-08-14 14:33:37 +0300

    new problem: add new problem 0823_3_1

---
+++ 0823_3/1_arrayTask.js
@@ -0,0 +1,12 @@
+//141 vs + 122 code
+//Create a method that takes as input a name, city, and state to welcome a person.
+//Note that name will be an array consisting of one or more values that should be joined together with one space between each,
+//and the length of the name array in test cases will vary.
+
+function sayHello( name, city, state ) {
+    return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`
+  }
+
+console.log(sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois')) 
+
+// Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!
