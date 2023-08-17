74b0a098f9552d1c382095b5b236ce9b6a75d60a
MrDrog <fireman3085@mail.ru>
2023-08-17 03:06:22 -0800

    new problem: add new problem 0823_3_5

---
+++ 0823_3/5_mapArr.js
@@ -0,0 +1,20 @@
+//what in console?
+
+const arr = [4, 5, 7, 8, 7, 5, 9]
+
+const newMap = arr.map(el => {
+    if (el%2 === 0) return el
+    else return el*2
+})
+
+console.log(arr)
+console.log(newMap)
+
+// [
+//     4, 5, 7, 8,
+//     7, 5, 9
+// ]
+// [
+//         4, 10, 14, 8,
+//     14, 10, 18
+// ]
