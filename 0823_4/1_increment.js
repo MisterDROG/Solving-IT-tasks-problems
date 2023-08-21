70aec9668afd02d75ef7c9a2830b2aebbb1a939f
MrDrog <fireman3085@mail.ru>
2023-08-21 19:45:00 +0300

    new problem: add new problem 0823_4_1

---
+++ 0823_4/1_increment.js
@@ -0,0 +1,14 @@
+//what in console?
+
+let num = 10;
+
+const increaseNumber = () => num++
+const increasePassedNumber = (number) => number++
+
+const num1 = increaseNumber()
+const num2 = increasePassedNumber(num1)
+
+console.log(num1)
+console.log(num2)
+
+// 10, 10
