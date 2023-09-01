7a6c6f042bfb9301ee16077f036f8346c07ebf20
MrDrog <fireman3085@mail.ru>
2023-09-01 16:22:22 +0300

    new problem: add fix to problem 0823_4_5

--- 0823_4/5_mail.js
+++ 0823_4/5_mail.js
@@ -12,3 +12,15 @@ console.log(user.email)
 
 
 // test@email.com
+
+const user2 = {
+    email: "test@email.com",
+    updateEmail: function(email) {
+        this.email = email
+    }
+}
+
+user2.updateEmail("newemail@mail.com")
+console.log(user2.email)
+
+// newemail@mail.com
