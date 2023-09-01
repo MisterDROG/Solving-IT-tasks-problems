37d8221ec91692186dc6b043e0d657ea9467ad0c
MrDrog <fireman3085@mail.ru>
2023-09-01 16:20:58 +0300

    new problem: add new problem 0823_4_5

---
+++ 0823_4/5_mail.js
@@ -0,0 +1,14 @@
+//what in console?
+
+const user = {
+    email: "test@email.com",
+    updateEmail: email => {
+        this.email = email
+    }
+}
+
+user.updateEmail("newemail@mail.com")
+console.log(user.email)
+
+
+// test@email.com
