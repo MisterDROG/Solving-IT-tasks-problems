e19e9a9210d10ce253c75584c7c0df6577b50593
MrDrog <fireman3085@mail.ru>
2023-09-07 22:11:04 +0300

    new problem: add new to problem 0923_1_3

---
+++ 0923_1/3_notTypeof.js
@@ -0,0 +1,15 @@
+// What in console?
+
+let randomValue =  { name : 'Tom'}
+
+randomValue = 23
+
+if (!typeof randomValue === 'string') {
+    console.log('Not a string')
+} else {
+    console.log('Its a string')
+}
+
+// !typeof randomValue === "string"
+// false === "string"
+// false
