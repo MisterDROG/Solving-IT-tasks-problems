60a4250393621e74c066d4daa7a8e16559908210
MrDrog <fireman3085@mail.ru>
2023-09-08 21:16:35 +0300

    new problem: add new to problem 0923_1_5

--- 0923_1/4_varProblem.js
+++ 0923_1/4_varProblem.js
@@ -8,5 +8,4 @@ for (let i = 0; i < 3; i++) {
     setTimeout(() => console.log(i), 1)
 }
 
-
 // 3 3 3 0 1 2
---
+++ 0923_1/5_equality.js
@@ -0,0 +1,15 @@
+// What in console?
+
+let a = 3
+
+let b = new Number(3)
+
+let c = 3 
+
+console.log(b)
+
+console.log(a == b)
+console.log(a === b)
+console.log(b === c)
+
+// true false false
