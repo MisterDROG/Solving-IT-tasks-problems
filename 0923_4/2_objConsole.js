2b89187f0715d17d5da8e9059b99a90435e20bfc
MrDrog <fireman3085@mail.ru>
2023-09-26 12:49:13 +0300

    new problem: add new to problem 0923_4_2

---
+++ 0923_4/2_objConsole.js
@@ -0,0 +1,14 @@
+//What in console?
+
+function sayHi() {
+
+    console.log(name)
+    console.log(age)
+    
+    var name = 'Todd'
+    let age = 21
+}
+
+sayHi()
+
+// undefined и ReferenceError
