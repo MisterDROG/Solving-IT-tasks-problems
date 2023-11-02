31716765ec5ef6f33537f9dc339498c594e74ee3
MrDrog <fireman3085@mail.ru>
2023-11-02 12:47:43 +0300

    new problems: add new to problem 1123_1_2

---
+++ 1123_1/2_makeTypes.ts
@@ -0,0 +1,90 @@
+//Вспомогательные функции
+const showTaskResult = (task: number, taskResult: { result: any, expectedResult: any }) => {
+    console.log(`---Результат задачи ${task}---`)
+    console.log('Ожидаемый: ', taskResult.expectedResult)
+    console.log('Из функции: ', taskResult.result)
+}
+
+// 1. TS
+//   Написать типизацию подходящую для двух объектов, не потерять типизацию "за" ключами в endpoints, то есть key:string и Record<string не подходят
+//   Принять что сами ключи endpoints нам известны на этапе типизации каждого объекта: 
+//   "getVtemplates" и "postVtemplates" для vtemplateObject
+//   "getReports" и "putReports" для reportObject
+
+//   const vtemplateObject:any = {
+//     entity: "vtemplate",
+//     endpoints: {
+//         getVtemplates: {
+//             method: "GET",
+//             url:  "vtemplate"
+//         },
+//         postVtemplates: {
+//             method: "POST",
+//             url:  "vtemplate"
+//         },
+//     }
+//   }
+//   const reportObject:any = {
+//     entity: "report",
+//     endpoints: {
+//         getReports: {
+//             method: "GET",
+//             url:  "report"
+//         },
+//         putReports: {
+//             method: "PUT",
+//             url:  "reports"
+//         },
+//     }
+//   }
+
+
+type HTTPType = "GET" | "POST" | "PUT";
+
+type Endpoint = {
+    method: HTTPType;
+    url: string;
+};
+
+type VTemplateEndpoints = {
+    getVtemplates: Endpoint;
+    postVtemplates: Endpoint;
+};
+
+type ReportEndpoints = {
+    getReports: Endpoint;
+    putReports: Endpoint;
+};
+
+type GeneralType = {
+    entity: string;
+    endpoints: VTemplateEndpoints | ReportEndpoints;
+};
+
+const vtemplateObject: GeneralType = {
+    entity: "vtemplate",
+    endpoints: {
+        getVtemplates: {
+            method: "GET",
+            url: "vtemplate",
+        },
+        postVtemplates: {
+            method: "POST",
+            url: "vtemplate",
+        },
+    },
+};
+
+const reportObject: GeneralType = {
+    entity: "report",
+    endpoints: {
+        getReports: {
+            method: "GET",
+            url: "report",
+        },
+        putReports: {
+            method: "PUT",
+            url: "reports",
+        },
+    },
+};
